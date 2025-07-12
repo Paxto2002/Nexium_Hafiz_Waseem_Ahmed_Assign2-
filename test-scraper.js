fbc7071e74df329d819bd950e50d196f2eef840a
Paxto2002 <hafizwaseemahmed2002@gmail.com>
2025-07-12 08:55:34 +0500

    fix: remove unnecessary files

--- test-scraper.js
+++
@@ -1,7 +0,0 @@
-import { scrapeBlogText } from "./lib/scraper.js";
-
-const url = "https://nextjs.org/docs/app/getting-started/installation";
-
-scrapeBlogText(url).then((text) => {
-  console.log("📄 Extracted Blog Content:\n", text.slice(0, 1000));
-});
